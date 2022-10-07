4a52659deea983753fa1079201ddeabbf95b5de5
Ahmad Ilham <ahmad.ilham@amartha.com>
2022-10-07 17:16:09 +0700

    handle font size

--- components/heros.js
+++ components/heros.js
@@ -1,14 +1,14 @@
 export default function Heros() {
     return (
         <div className="bg-gray-200">
-            <div className="max-w-7xl mx-auto py-36 relative">
+            <div className="max-w-7xl mx-auto md:py-36 py-28 relative">
                 <div className="absolute w-full -top-[43px] md:-top-28 left-0 px-5 md:px-0">
                     <div className="flex flex-col md:flex-row md:items-center space-y-10 justify-between">
-                        <div className="font-black text-7xl md:text-9xl text-black shadow-xl px-3 py-3">
+                        <div className="font-black text-5xl md:text-9xl text-black shadow-xl px-3 py-3">
                             <h1>Our deep</h1>
                             <div className="flex items-center space-x-5">
                                 <h1>story.</h1>
-                                <p className="text-sm md:text-base max-w-xs font-medium">Still, the second option holds promise could make the tagline shortlist if you are delivering relaxed.</p>
+                                <p className="text-xs md:text-base max-w-xs font-medium">Still, the second option holds promise could make the tagline shortlist.</p>
                             </div>
                         </div>
                         <div className="grid grid-flow-col md:grid-flow-row">
--- components/services.js
+++ components/services.js
@@ -6,36 +6,36 @@ import CreditScoreRoundedIcon from '@mui/icons-material/CreditScoreRounded';
 export default function Services() {
     return (
         <div className="bg-gray-200">
-            <div className="max-w-5xl mx-auto px-5 py-10">
+            <div className="max-w-5xl mx-auto px-5 md:py-10 py-5">
                 <div className="grid grid-flow-col gap-2">
                     <div className="rounded col-span-2 row-span-2 group bg-gray-300/20">
-                        <div className="flex w-full h-full py-2 text-center text-sm text-green-800 group-hover:text-green-800/50 duration-200 flex-col space-y-1 justify-center items-center">
+                        <div className="flex w-full h-full py-2 text-center md:text-sm text-xs text-green-800 group-hover:text-green-800/50 duration-200 flex-col space-y-1 justify-center items-center">
                             <div className='bg-gray-300/50 rounded-full p-2 flex items-center justify-center'>
-                                <FastForwardRoundedIcon fontSize='large'/>
+                                <FastForwardRoundedIcon fontSize='md:large'/>
                             </div>
                             <p>Super Fast handling</p>
                         </div>
                     </div>
                     <div className="bg-gray-300/20  rounded col-span-2 row-span-1 md:col-span-1 group">
-                        <div className="flex  w-full h-full py-2 px-2 text-left text-sm text-green-800 group-hover:text-green-800/50 duration-200 items-center">
+                        <div className="flex  w-full h-full py-2 px-2 text-left md:text-sm text-xs text-green-800 group-hover:text-green-800/50 duration-200 items-center">
                             <div className='bg-gray-300/50 rounded-full p-2 mr-3 flex items-center justify-center'>
-                                <ShieldRoundedIcon fontSize='large'/>
+                                <ShieldRoundedIcon fontSize='md:large'/>
                             </div>
                             <p>Secured Data</p>
                         </div>
                     </div>
                     <div className="bg-gray-300/20  rounded col-span-2 md:col-span-1 row-span-1 group">
-                        <div className="flex w-full h-full py-2 px-2 text-left text-sm text-green-800 items-center group-hover:text-green-800/50 duration-200">
+                        <div className="flex w-full h-full py-2 px-2 text-left md:text-sm text-xs text-green-800 items-center group-hover:text-green-800/50 duration-200">
                             <div className='bg-gray-300/50 rounded-full p-2 mr-3 flex items-center justify-center'>
-                                <CurrencyExchangeRoundedIcon fontSize='large'/>
+                                <CurrencyExchangeRoundedIcon fontSize='md:large'/>
                             </div>
                             <p>Money-back guarantee</p>
                         </div>
                     </div>
                     <div className="bg-gray-300/20  rounded col-span-2 row-span-2 group">
-                        <div className="flex w-full h-full py-2 text-center text-sm text-green-800 flex-col space-y-1 justify-center items-center group-hover:text-green-800/50 duration-200">
+                        <div className="flex w-full h-full py-2 text-center md:text-sm text-xs text-green-800 flex-col space-y-1 justify-center items-center group-hover:text-green-800/50 duration-200">
                             <div className='bg-gray-300/50 rounded-full p-2 flex items-center justify-center'>
-                                <CreditScoreRoundedIcon fontSize='large'/>
+                                <CreditScoreRoundedIcon fontSize='md:large'/>
                             </div>
                             <p>Super secure payment system</p>
                         </div>
